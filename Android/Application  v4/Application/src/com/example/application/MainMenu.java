adefb3616d428e1a8953b05a227392cd574c9597
Tran Nhat Sang <sang004@e.ntu.edu.sg>
2015-02-09 20:37:32 +0800

    Update the main menu

--- Android/Application  v4/Application/src/com/example/application/MainMenu.java
+++ Android/Application  v4/Application/src/com/example/application/MainMenu.java
@@ -7,6 +7,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainMenu extends ActionBarActivity {
@@ -38,6 +39,47 @@ public class MainMenu extends ActionBarActivity {
         		startActivity(i);
         	}
         });    
+        
+        //whatIsThis text view
+        Button bluetoothApplicationsButton = (Button) findViewById(R.id.bluetoothApplicationsButton);
+        Button persistentConfigurationButton = (Button) findViewById(R.id.persistentConfigurationButton);
+        Button beyondTheBasicsButton = (Button) findViewById(R.id.beyondTheBasicsButton);
+        final TextView whatIsThisTextView = (TextView) findViewById(R.id.whatIsThisTextView);
+        
+        bluetoothApplicationsButton.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				//TextView whatIsThisTextView = (TextView) findViewById(R.id.whatIsThisTextView);
+				whatIsThisTextView.setText("\n\n\n" +
+				                           "Task C.1 - Tansmit and receive text strings over the Bluetooth communication link." + "\n" +
+				                           "Task C.2 - Scan, select and connect Bluetooth device." + "\n" + 
+						                   "Task C.3 - Navigate robot forward, left and right." + "\n" + 
+				                           "Task C.4 - Indicate current status of robot." + "\n" + 
+						                   "Task C.5 - Update Robot Start Coordinates to PC." + "\n" + 
+				                           "Task C.6 - 2D display of maze environment." + "\n" + 
+						                   "Task C.7 - Manual /  Auto graphical update to maze." + "\n" + 
+				                           "Task C.9 - Robust connectivity with Bluetooth device.");
+				
+			}
+		});
+        
+        persistentConfigurationButton.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				whatIsThisTextView.setText("\n\n\n" + "Task C.8 - Persistent user configurable commands to robot.");
+			}
+		});
+        
+        beyondTheBasicsButton.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				whatIsThisTextView.setText("\n\n\n" + "Task C.10 - Extension beyond the basics. (3D map??, Voice Command??, tilt screen to move robot??)");
+			}
+		});
+        
 	}
 
 	@Override
@@ -63,7 +105,14 @@ public class MainMenu extends ActionBarActivity {
   	//display pop-up
   	public void questionc1(View view)
   	{
-  	    Toast.makeText(this, "Task C.1 - Transmit & receive text strings over the Bluetooth comm link." + "\n" + "Task C.2 - Scan, select & connect with Bluetooth device." + "\n" + "Task C.3 - Navigate robot Up, Down, Left, Right." + "\n" + "Task C.4 - Indicate current status of robot." + "\n" + "Task C.5 - 2D display of maze environment." + "\n" + "Task C.6 - Manual / Auto graphical update to maze." + "\n" + "Task C.8 - Robust connectivity with Bluetooth device." + "\n" + "Task C.9 - Extras: Continuous touch control & Tilt sensing", Toast.LENGTH_LONG).show();
+  	    Toast.makeText(this, "Task C.1 - Transmit & receive text strings over the Bluetooth comm link." + "\n" + 
+  	                         "Task C.2 - Scan, select & connect with Bluetooth device." + "\n" + 
+  	    		             "Task C.3 - Navigate robot Up, Down, Left, Right." + "\n" + 
+  	                         "Task C.4 - Indicate current status of robot." + "\n" + 
+  	    		             "Task C.5 - 2D display of maze environment." + "\n" + 
+  	                         "Task C.6 - Manual / Auto graphical update to maze." + "\n" + 
+  	    		             "Task C.8 - Robust connectivity with Bluetooth device." + "\n" + 
+  	                         "Task C.9 - Extras: Continuous touch control & Tilt sensing", Toast.LENGTH_LONG).show();
   	}
 
   	public void questionc2(View view)
