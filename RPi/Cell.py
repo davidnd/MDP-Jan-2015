c3511d1ce6cc50a95f0044e555c0f6061a230534
PhucNguyen <phuc10cdt2@gmail.com>
2015-03-06 11:55:28 +0800

    Set up rpi

---
+++ RPi/Cell.py
@@ -0,0 +1,10 @@
+# To change this license header, choose License Headers in Project Properties.
+# To change this template file, choose Tools | Templates
+# and open the template in the editor.
+
+class Cell:
+    
+    def __init__(self):
+        self.status=0
+  #  def __init__(self, st):
+   #     self.status=st
